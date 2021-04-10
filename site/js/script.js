ff397bb372c8bee5ceddacfe49f2fa753afd4993
kushagra rana <rkushagra777@gmail.com>
2021-04-10 22:15:19 +0530

    http to https

--- site/js/script.js
+++ site/js/script.js
@@ -11,11 +11,11 @@ $(function () {
     var homeHtml = "snippets/home-snippet.html";
     //convenience function for inserting innerHTML for select
     var allCategoriesUrl = 
-    "http://davids-restaurant.herokuapp.com/categories.json";
+    "https://davids-restaurant.herokuapp.com/categories.json";
     var categoriesTitleHtml = "snippets/categories-title-snippet.html";
     var categoryHtml = "snippets/category-snippet.html";
     var menuItemsUrl = 
-    "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+    "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
     var menuItemsTitleHtml = "snippets/menu-items-title.html";
     var menuItemHtml = "snippets/menu-item.html";
 
